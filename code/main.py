2f0d0af909873d6bb00da23a0d1dfd3959142da9
Sev Leonard <jennifer.m.leonard@gmail.com>
2017-05-17 17:13:17 -0700

    adding mqtt polling

--- code/main.py
+++ code/main.py
@@ -4,7 +4,6 @@ from umqtt.simple import MQTTClient
 
 c = MQTTClient("umqtt_client",'192.168.4.2')
 my_dht = dht.DHT11(machine.Pin(2))
-mqtt_connected = False
 
 def measure_temp_humidity(temp_outfile, humidity_outfile, poll_time_s):
     while True:
@@ -20,28 +19,27 @@ def measure_temp_humidity(temp_outfile, humidity_outfile, poll_time_s):
         time.sleep(poll_time_s)
 
 def connect_mqtt():
-    if not(mqtt_connected):
-        try:
-            c.connect()
-            mqtt_connected = True
+    try:
+        res = c.connect()
+        if res == 0:
             return True
-        except Exception as ex:
-            print('unable to connect mqtt:', ex.message)
-            return False
+    except Exception as ex:
+        print('unable to connect mqtt:', ex.message)
+        return False
 
-def disconnect_mqtt():
-    if (mqtt_connected):
-        c.disconnect()
-        mqtt_connected = False
 
 def temp_humidity_mqtt(poll_time_s):
-    #if connect_mqtt():
-    while True:
-        my_dht.measure()
-        temp = my_dht.temperature()
-        humidity = my_dht.humidity()
-        c.publish(b"/vine_sensor/temp",str(temp))
-        c.publish(b"/vine_sensor/humidity",str(humidity))
-        print("temp: ", temp)
-        print("humidity: ", humidity)
-        time.sleep(poll_time_s)
+    if connect_mqtt():
+        while True:
+            my_dht.measure()
+            temp = my_dht.temperature()
+            humidity = my_dht.humidity()
+            try:
+                c.publish(b"/vine_sensor/temp",str(temp))
+                c.publish(b"/vine_sensor/humidity",str(humidity))
+            except Exception as ex:
+                print("unable to publish to MQTT:", ex.message)
+                return
+            print("temp: ", temp)
+            print("humidity: ", humidity)
+            time.sleep(poll_time_s)
